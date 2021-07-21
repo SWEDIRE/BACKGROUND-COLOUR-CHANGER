517693b000188f4915756351aa1cfeac018bd329
SWEDIRE <86052247+SWEDIRE@users.noreply.github.com>
2021-07-21 15:48:23 +0530

    Add files via upload

---
+++ p5play_template-main/p5play_template-main/sketch.js
@@ -0,0 +1,25 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(30);
+}
+
+function draw() 
+{
+if (keyIsDown (UP_ARROW)) {
+background ("green")
+}
+if (keyIsDown (DOWN_ARROW)) {
+  background ("red")
+}
+if (keyIsDown (RIGHT_ARROW)) {
+  background ("blue")
+}
+if (keyIsDown (LEFT_ARROW)) {
+  background ("yellow")
+}
+}
+
+
+
+
